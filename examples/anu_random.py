f58244c75b92984ea048b1dbb5fcc35531f5c658
John Chase <ratteal@gmail.com>
2019-07-08 14:49:34 +0100

    Refactor to blank out if the service isn't available.

--- examples/anu_random.py
+++ examples/anu_random.py
@@ -5,17 +5,32 @@
 import ledshim, requests, json
 from time import sleep
 
+
+def get_random_numbers():
+    try:
+        response = requests.get("https://qrng.anu.edu.au/API/jsonI.php?type=hex16&length=28&size=3",  timeout=3)
+        response.raise_for_status()
+        anu_json = json.loads(response.text)
+        return anu_json["data"]
+    except requests.exceptions.RequestException:
+        return
+
+
+def render_pixels():
+    for i in range(ledshim.NUM_PIXELS):
+        pixel = bytearray.fromhex(data[i])
+        print(pixel[0], pixel[1], pixel[2])
+        ledshim.set_pixel(i, pixel[0], pixel[1], pixel[2])
+
+
 ledshim.set_clear_on_exit()
 
 while True:
-    response = requests.get("https://qrng.anu.edu.au/API/jsonI.php?type=hex16&length=28&size=3",  timeout=3)
-    if response.status_code == 200:
-        anu_json = json.loads(response.text)
-        data = anu_json["data"]
-        for i in range(ledshim.NUM_PIXELS):
-            pixel = bytearray.fromhex(data[i])
-            print(pixel[0], pixel[1], pixel[2])
-            ledshim.set_pixel(i, pixel[0], pixel[1], pixel[2])
+    data = get_random_numbers()
+    if data is None:
+        ledshim.clear()
+    else:
+        render_pixels()
         ledshim.show()
     print("Blink!")
     sleep(1)
